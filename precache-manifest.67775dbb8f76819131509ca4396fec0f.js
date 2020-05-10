826c6d44fac03af0a65c725de5def11b3d884a4b
sotomaque <sotomaque@gmail.com>
2020-05-09 21:42:13 -0700

    Updates

---
+++ precache-manifest.67775dbb8f76819131509ca4396fec0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81568072614845a0746bc00bb444ba1d",
+    "url": "/React-IMDB/index.html"
+  },
+  {
+    "revision": "8c152e5a2cb3c2580608",
+    "url": "/React-IMDB/static/js/2.f5715ffd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-IMDB/static/js/2.f5715ffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89736052349697b90d89",
+    "url": "/React-IMDB/static/js/main.6fc5da9d.chunk.js"
+  },
+  {
+    "revision": "8d456ea8752a3889f2f3",
+    "url": "/React-IMDB/static/js/runtime-main.712a69aa.js"
+  },
+  {
+    "revision": "d9247b29d21118325ff3d2a007dcebd1",
+    "url": "/React-IMDB/static/media/netflix.d9247b29.png"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/React-IMDB/static/media/no_image.31f5bb1d.jpg"
+  }
+]);
