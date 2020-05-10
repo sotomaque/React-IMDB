069db8f1062e7f49dc041ea5f767cc2d908b775c
sotomaque <sotomaque@gmail.com>
2020-05-10 01:50:14 -0700

    trying to fix issue where gitpages doesnt take u to my 404 page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // Components
 import Header from './components/elements/Header';
@@ -37,7 +37,7 @@ const App = () => (
             <Route exact path='/actors/:actorId' component={ActorPage} />
 
             <Route path="/404" component={NotFoundPage} />
-            <Redirect to="/404" />
+            <Route path="*" component={NotFoundPage} />
  
         </Switch>
         <GlobalStyle />
