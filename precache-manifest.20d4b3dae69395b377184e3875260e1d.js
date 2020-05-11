1fcfb646e111a800bf8c8b3a36d5a713255b66d3
sotomaque <sotomaque@gmail.com>
2020-05-10 19:38:50 -0700

    Updates

--- precache-manifest.20d4b3dae69395b377184e3875260e1d.js
+++ precache-manifest.20d4b3dae69395b377184e3875260e1d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "78268bc0d0cc5a35222b7de006103f2c",
+    "revision": "b4595890917b1dd65f96ffa9d3a68ff8",
     "url": "/React-IMDB/index.html"
   },
   {
-    "revision": "37fe972f5cb6c4870f58",
-    "url": "/React-IMDB/static/js/2.842c4dc5.chunk.js"
+    "revision": "f66548dcc68c6ddbc741",
+    "url": "/React-IMDB/static/js/2.fd7db1f3.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/React-IMDB/static/js/2.842c4dc5.chunk.js.LICENSE.txt"
+    "url": "/React-IMDB/static/js/2.fd7db1f3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dcf313358a5d9263e1f3",
-    "url": "/React-IMDB/static/js/main.dda611e0.chunk.js"
+    "revision": "a16ee781c119eeee53cb",
+    "url": "/React-IMDB/static/js/main.b1ae6e84.chunk.js"
   },
   {
     "revision": "8d456ea8752a3889f2f3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-IMDB/precache-manifest.06ce01c86edd6a6cbe33d696f166b414.js"
+  "/React-IMDB/precache-manifest.20d4b3dae69395b377184e3875260e1d.js"
 );
 
 self.addEventListener('message', (event) => {
