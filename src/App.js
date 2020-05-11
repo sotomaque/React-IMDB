d05c52472c7d68cef4aad07260468dd4f20444d1
sotomaque <sotomaque@gmail.com>
2020-05-10 19:38:29 -0700

    still unable to see 404 page on hosted site;

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 // Components
 import Header from './components/Navigation/Header';
@@ -32,6 +32,8 @@ const App = () => (
             <Route exact path='/React-IMDB/people/:actorId' component={ActorPage} />
 
             <Route path="/React-IMDB/404" component={NotFoundPage} />
+            <Redirect to='/React-IMDB' />
+            
             <Route path="*" component={NotFoundPage} />
  
         </Switch>
